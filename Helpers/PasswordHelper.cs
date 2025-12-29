c13242b0805c0a211d55ec4fbc04cc05dd4fe503
Srikar4123 <pssrikar02@gmail.com>
2025-12-29 21:29:14 +0530

    Commit

---
+++ Helpers/PasswordHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LibraryManagementBE.Helpers
+{
+    public static class PasswordHelper
+    {
+        private static readonly PasswordHasher<string> hasher = new();
+
+        public static string Hash(string password)
+        {
+            return hasher.HashPassword(null, password);
+        }
+
+        public static bool Verify(string hash, string input)
+        {
+            return hasher.VerifyHashedPassword(null, hash, input)
+                   == PasswordVerificationResult.Success;
+        }
+    }
+}
